28f5e21016cb079e4e59d5f28672de1f15a66476
Nitin <52646268+Nitin1818@users.noreply.github.com>
2020-05-24 09:52:11 +0530

    ;-;

--- haruka/modules/warns.py
+++ haruka/modules/warns.py
@@ -29,6 +29,9 @@ def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = N
     if is_user_admin(chat, user.id):
         message.reply_text("I'm not going to warn an admin!")
         return ""
+    if user_id == 777000:
+        message.reply_text("I can't warn tg ;-;")
+        return ""
 
     if warner:
         warner_tag = mention_html(warner.id, warner.first_name)
