e92f8c7db426e49bcab27ea97eeb1eafd4ee59ed
Nitin <52646268+Nitin1818@users.noreply.github.com>
2020-05-27 13:05:33 +0530

    Mention CHAT NAME
    Mention chat name when trying to enable or disable cmds

--- haruka/modules/disable.py
+++ haruka/modules/disable.py
@@ -75,7 +75,7 @@ if is_module_loaded(FILENAME):
 
             if disable_cmd in set(DISABLE_CMDS + DISABLE_OTHER):
                 sql.disable_command(chat.id, disable_cmd)
-                update.effective_message.reply_text("Disabled the use of `{}`".format(disable_cmd),
+                update.effective_message.reply_text("Disabled the use of '`{cmd}`' in *{chat}*".format(cmd=disable_cmd, chat=chat.title),
                                                     parse_mode=ParseMode.MARKDOWN)
             else:
                 update.effective_message.reply_text("That command can't be disabled")
@@ -94,7 +94,7 @@ if is_module_loaded(FILENAME):
                 enable_cmd = enable_cmd[1:]
 
             if sql.enable_command(chat.id, enable_cmd):
-                update.effective_message.reply_text("Enabled the use of `{}`".format(enable_cmd),
+                update.effective_message.reply_text("Enabled the use of '`{cmd}`' in *{chat}*".format(cmd=enable_cmd, chat=chat.title),
                                                     parse_mode=ParseMode.MARKDOWN)
             else:
                 update.effective_message.reply_text("Is that even disabled?")
